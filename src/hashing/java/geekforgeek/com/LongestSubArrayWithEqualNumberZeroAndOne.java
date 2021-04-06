2c5ebf244e19d2e7a7fb5dd4745bcd14c5e7f853
Ashivani <ashivani.singh2017@gmail.com>
2021-04-06 23:45:14 +0530

    my first commit data structure algorithm question solve by me on date 6-4-2021....

---
+++ src/hashing/java/geekforgeek/com/LongestSubArrayWithEqualNumberZeroAndOne.java
@@ -0,0 +1,35 @@
+package hashing.java.geekforgeek.com;
+
+import java.util.HashMap;
+
+public class LongestSubArrayWithEqualNumberZeroAndOne {
+
+    // simple trick in this problem assume o as -1
+    // and calculate just like longest sum by given some
+    //
+    public static int calculateLengthOfSubArray(int arr[]){
+        int maxCount=0,prefixSum=0;
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]==0){
+                prefixSum+=-1;
+            }
+            prefixSum+=arr[i];
+            if(prefixSum==0){
+                maxCount=Math.max(maxCount,i+1);
+            }
+            if(!hashMap.containsKey(prefixSum)){
+                hashMap.put(prefixSum,i);
+            }
+            if(hashMap.containsKey(prefixSum)){
+                maxCount=Math.max(maxCount,i-hashMap.get(prefixSum));
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String [] args){
+        int arr[]=new int[]{1,0,1,1,1,0,0};
+        System.out.println(calculateLengthOfSubArray(arr));
+    }
+}
