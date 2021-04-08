d4a77c02f578839785fa54853b53cd7b6e610548
Ashivani <ashivani.singh2017@gmail.com>
2021-04-08 23:28:59 +0530

    my first commit data structure algorithm question solve by me on date 8-4-2021....

---
+++ src/hashing/java/geekforgeek/com/LongestCommonSpanWithSameSumInTwoBinaryArrayAnotherNaiveApproach.java
@@ -0,0 +1,29 @@
+package hashing.java.geekforgeek.com;
+
+public class LongestCommonSpanWithSameSumInTwoBinaryArrayAnotherNaiveApproach {
+
+    public static int calculateLongestCommonSpanWithSameSumInTwoBinary(int arr1[],int arr2[]){
+        int sum1=0,sum2=0;
+        int maxCont=0;
+        for(int i=0;i<arr1.length;i++){
+            sum1=0;
+            sum2=0;
+            for(int j=i;j<arr1.length;j++){
+                sum1+=arr1[i];
+                sum2+=arr2[i];
+
+                if(sum1==sum2){
+                    maxCont=Math.max(maxCont,j-i+1);
+                }
+            }
+        }
+        return maxCont;
+    }
+
+
+    public static void main(String[] args) {
+        int arr1[]=new int[]{0,1,0,0,0,0};
+        int arr2[]=new int[]{1,0,1,0,0,1};
+        System.out.println(calculateLongestCommonSpanWithSameSumInTwoBinary(arr1,arr2));
+    }
+}
