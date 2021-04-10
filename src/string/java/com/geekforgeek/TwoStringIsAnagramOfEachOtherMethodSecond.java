cd0518a94574a14788d9c3d7e26e59f1997a17d6
Ashivani <ashivani.singh2017@gmail.com>
2021-04-10 20:53:21 +0530

    my second commit data structure algorithm question solve by me on date 10-4-2021....

---
+++ src/string/java/com/geekforgeek/TwoStringIsAnagramOfEachOtherMethodSecond.java
@@ -0,0 +1,39 @@
+package string.java.com.geekforgeek;
+
+public class TwoStringIsAnagramOfEachOtherMethodSecond {
+    // second method creating 256 value of ascii value of empty value array
+    // what we get character that time increase the value
+    // of count integer in the array
+    //and iterate the second string each time what value in second string decrease that value
+    // after iterating second string we check in array any value is not zero
+    // if we get any point not zero then return false
+    // otherwise return true
+
+    public static boolean checkTwoStringIsAnagram(String str1,String str2){
+        if(str1.length()!=str2.length())
+            return false;
+        int count[]=new int[256];
+
+        for(int i=0;i<str1.length();i++){
+            count[str1.charAt(i)]++;
+        }
+
+        for(int i=0;i<str2.length();i++){
+            count[str2.charAt(i)]--;
+        }
+        for(int i=0;i<256;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1="geeks";
+        String str2="ekegs";
+//        String str1="aabcd";
+//        String str2="abbcd";
+        System.out.println(checkTwoStringIsAnagram(str1,str2));
+    }
+}
