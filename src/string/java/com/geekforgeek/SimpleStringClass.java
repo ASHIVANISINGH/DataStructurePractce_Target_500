3402dafc284de8da9968d9d6d629ff22b2f3a87c
Ashivani <ashivani.singh2017@gmail.com>
2021-04-13 23:43:38 +0530

    first commit on 13/04/1999

--- src/string/java/com/geekforgeek/SimpleStringClass.java
+++ src/string/java/com/geekforgeek/SimpleStringClass.java
@@ -1,4 +1,16 @@
 package string.java.com.geekforgeek;
 
 public class SimpleStringClass {
+
+    public static void main(String[] args) {
+        String s1 = "Ram";
+        String s2 = "Ram";
+        String s3 = "Shyam";
+        String s4 = "ABC";
+
+        System.out.println(" Comparing strings with compareTo:");
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
 }
