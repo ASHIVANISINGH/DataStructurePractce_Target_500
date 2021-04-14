bf81af79131b69692628fd89aa5799f6c8235de8
Ashivani <ashivani.singh2017@gmail.com>
2021-04-14 23:49:55 +0530

    my first commit data structure algorithm question solve by me on date 14-4-2021....

--- src/string/java/com/geekforgeek/pratice/LexographicString.java
+++ src/string/java/com/geekforgeek/pratice/LexographicString.java
@@ -3,27 +3,27 @@ package string.java.com.geekforgeek.pratice;
 public class LexographicString {
 
     public int rankOfStringInLexographic(String str){
-//        int rank=1;
-//        int n=str.length();
-//        int mal=fuct(n);
-//        System.out.println(""+mal);
-//        int[] count=new int[256];
-//        char []arr=str.toCharArray();
-//        for(int i=0;i<arr.length;i++){
-//            count[arr[i]]++;
-//        }
-//        for(int i=1;i<n;i++){
-//            count[i]=count[i]+count[i-1];
-//        }
-//        for(int i=0;i<n;i++){
-//            mal=mal/(n-i);
-//            rank=rank+count[arr[i]-1]*mal;
-//            for(int j=arr[i];j<256;j++) {
-//                count[j]--;
-//            }
-//        }
-//        return rank+1;
-        return 0;
+        int rank=1;
+        int n=str.length();
+        int mal=fuct(n);
+        System.out.println(""+mal);
+        int[] count=new int[256];
+        char []arr=str.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        for(int i=1;i<n;i++){
+            count[i]=count[i]+count[i-1];
+        }
+        for(int i=0;i<n;i++){
+            mal=mal/(n-i);
+            rank=rank+count[arr[i]-1]*mal;
+            for(int j=arr[i];j<256;j++) {
+                count[j]--;
+            }
+        }
+       return rank+1;
+       // return 0;
     }
 
     public static int fuct(int n){
