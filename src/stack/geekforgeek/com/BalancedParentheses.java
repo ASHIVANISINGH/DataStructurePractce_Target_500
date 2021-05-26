3d4cb18a894eac0cd9d9cf6dcd09a996f46eca88
Ashivani <ashivani.singh2017@gmail.com>
2021-05-26 15:02:42 +0530

    my first commit data structure algorithm question solve by me on date 26-4-2021....

--- src/stack/geekforgeek/com/BalancedParentheses.java
+++ src/stack/geekforgeek/com/BalancedParentheses.java
@@ -4,15 +4,23 @@ import java.util.Stack;
 
 public class BalancedParentheses {
 
+    // approach is simple
+    // if opening bracket we get then  we push it on stack
+    // else if we get closing bracket then we which bracket we get
+    // and check matching with top of stack if match is found return false
+
     public static boolean checkBalanceParentheses(String str){
+        // creating stack
         Stack<Character> stack=new Stack<>();
 
         for(char c : str.toCharArray()){
+            // checking opening bracket
             if(c=='('||c=='{'||c=='['){
                 stack.push(c);
             }else {
+                // check closing bracket
                 if(c!=')' && c != '}' ){
-                    if(stack.peek()=='['){
+                    if(!stack.isEmpty()&&stack.peek()=='['){
                         stack.pop();
                     }else {
                         return false;
@@ -20,7 +28,7 @@ public class BalancedParentheses {
                 }
 
                 if(c!=')' && c != ']' ){
-                    if(stack.peek()=='{'){
+                    if(!stack.isEmpty()&&stack.peek()=='{'){
                         stack.pop();
                     }else {
                         return false;
@@ -28,7 +36,7 @@ public class BalancedParentheses {
                 }
 
                 if(c!=']' && c != '}' ){
-                    if(stack.peek()=='('){
+                    if(!stack.isEmpty()&&stack.peek()=='('){
                         stack.pop();
                     }else {
                         return false;
@@ -36,11 +44,15 @@ public class BalancedParentheses {
                 }
             }
         }
+        // if stack is empty then that means every parentheses match found , if
+        // stack is not empty then that means parentheses not match
         return (stack.isEmpty() ) ? true : false;
     }
 
     public static void main(String[] args) {
-        String str="(({[]}))";
+       // String str="(({[]}))";
+        String str= "[()]{}{[()()]()}";
+
         System.out.println(" "+checkBalanceParentheses(str));
     }
 }
